9d773c671ec9160b90db42a8c2820798a7161943
Steve Cui (Wipro Ltd) <v-hocui@microsoft.com>
2018-11-08 17:41:18 -0800

    make slave_enable_netpoll() running forcely to have a try

--- hv-rhel6.x/hv/netvsc_drv.c
+++ hv-rhel6.x/hv/netvsc_drv.c
@@ -2102,7 +2102,7 @@ int netvsc_bond_enslave(struct net_device *bond_dev, struct net_device *slave_de
 	slave_dev->npinfo = bond->dev->npinfo;
 	printk("hello_napi:slave_dev->npinfo:%lx\n",(uintptr_t)(slave_dev->npinfo));
 
-	if (slave_dev->npinfo) {
+	//if (slave_dev->npinfo) {
 		printk("hello_napi\n");
 		//if (slave_enable_netpoll(new_slave)) {
 		if (slave_enable_netpoll(slave_dev)) {
