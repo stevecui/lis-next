98dee85aeb2b6d598502a9f17b99130bf527cb96
Steve Cui (Wipro Ltd) <v-hocui@microsoft.com>
2018-11-16 10:34:18 -0800

    deleted napi_reuse_skb(napi,skb)

--- hv-rhel6.x/hv/netvsc_drv.c
+++ hv-rhel6.x/hv/netvsc_drv.c
@@ -714,7 +714,7 @@ static struct sk_buff *netvsc_alloc_recv_skb(struct net_device *net,
 #else
 	skb = netdev_alloc_skb_ip_align(net, buflen);
 #endif
-	napi_reuse_skb(napi,skb);
+	//napi_reuse_skb(napi,skb);
 
 	if (!skb)
 		return skb;
