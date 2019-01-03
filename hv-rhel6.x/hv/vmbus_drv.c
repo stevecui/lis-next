228149769e30924b295a7f8cf89cc9c8216a7c4f
Steve Cui (Wipro Ltd) <v-hocui@microsoft.com>
2019-01-03 00:05:10 -0800

    deleted a printk statement

--- hv-rhel6.x/hv/vmbus_drv.c
+++ hv-rhel6.x/hv/vmbus_drv.c
@@ -752,7 +752,6 @@ int vmbus_add_channel_kobj(struct hv_device *dev, struct vmbus_channel *channel)
                 return ret;
 
         kobject_uevent(kobj, KOBJ_ADD);
-        printk("wmdsj:cpu:%d,dev_id:%x,ven_id:%x\n",smp_processor_id(),channel->device_id,channel->vendor_id);
         return 0;
 }
 
